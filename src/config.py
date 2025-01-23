12bd7a8132ac9db646276a6572202692a3d93868
Rostik <rosts@mail.ru>
2025-01-23 11:15:18 +0300

    changes in two files: config.py , base.py

--- src/config.py
+++ src/config.py
@@ -1,11 +1,31 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class DBSettings(BaseSettings):
+    db_url: str = "sqlite+aiosqlite:///./tasks.db"
+
 
 class Settings(BaseSettings):
-    db_url: str = "sqlite+aiosqlite:///./tasks.db"
+    db: DBSettings = DBSettings()
+
+
     class Config:
         env_file = ".env"
+        model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
 
 config = Settings()
 
 
+
+#**************************************************************************************************
+
+# from pydantic_settings import BaseSettings
+#
+# class Settings(BaseSettings):
+#     db_url: str = "sqlite+aiosqlite:///./tasks.db"
+#     class Config:
+#         env_file = ".env"
+#
+#
+# config = Settings()
--- src/db/base.py
+++ src/db/base.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncEngine, async_sessionmaker, create_async
 from config import config
 
 
-engine: AsyncEngine = create_async_engine(config.db_url)
+engine: AsyncEngine = create_async_engine(config.db.db_url)
 AsyncSessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
 
 
